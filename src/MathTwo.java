0fccf571d89327078bfcb646fc109b55c12a322e
GBTC441013UR <GBTC441013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-04 15:01:30 -0400

    Math two

---
+++ src/MathTwo.java
@@ -0,0 +1,9 @@
+public class MathTwo {
+
+    public static void main(String[] args) {
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println("My first number is: "+ mynumber);
+        System.out.println("My second number is: " + myothernumber);
+    }
+}
